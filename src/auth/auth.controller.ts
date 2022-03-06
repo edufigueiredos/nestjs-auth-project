e5a071e774bca95f032d178210d0a7cd474beceb
edufigueiredos <eduardo_figueiredo@id.uff.br>
2022-03-06 20:49:50 -0300

    Starting the auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() body: { email: string, senha: string }) {
+    return this.authService.login(body);
+  }
+}
---
+++ src/environment/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  jwt_secret: 'senhaSuperSegura',
+  jwt_expire: '30d'
+}
